ddb5de4c655c5a307b3df22635bbd9f621d5eded
Arnau Bigas <arnau.bigas@bsc.es>
2025-03-28 15:44:55 +0100

    Add byte-mask memory from Memory Library

--- rtl/src/common/hpdcache_sram_wbyteenable.sv
+++ rtl/src/common/hpdcache_sram_wbyteenable.sv
@@ -40,19 +40,32 @@ module hpdcache_sram_wbyteenable
     output logic [DATA_SIZE-1:0]   rdata
 );
 
-    hpdcache_sram_wbyteenable_1rw #(
-        .ADDR_SIZE(ADDR_SIZE),
-        .DATA_SIZE(DATA_SIZE),
-        .DEPTH(DEPTH)
-    ) ram_i (
-        .clk,
-        .rst_n,
-        .cs,
-        .we,
-        .addr,
-        .wdata,
-        .wbyteenable,
-        .rdata
+    sp_ram #(
+        .ADDR_WIDTH(ADDR_SIZE),
+        .DATA_WIDTH(DATA_SIZE),
+        `ifdef SRAM_IP
+        .INSTANTIATE_ASIC_MEMORY(1'b1),
+        `else
+        .INSTANTIATE_ASIC_MEMORY(1'b0),
+        `endif
+        .COL_WIDTH(8),
+        .INIT_MEMORY_ON_RESET('0) // HPDC doesn't initialize any SRAM
+    ) sram (
+        `ifdef INTEL_PHYSICAL_MEM_CTRL
+        .INTEL_MEM_CTRL(uhdusplr_mem_ctrl),
+        `endif
+        .SR_ID('0),
+        .clk(clk),
+        .rst_n(rst_n),
+        .clk_en(cs),
+        .rdw_en(we),
+        .addr(addr),
+        .data_in(wdata),
+        .data_mask_in(wbyteenable),
+        .data_out(rdata),
+        .srams_rtap_data( /* Unconnected */ ),
+        .rtap_srams_bist_command('0),
+        .rtap_srams_bist_data('0)
     );
 
 endmodule : hpdcache_sram_wbyteenable
